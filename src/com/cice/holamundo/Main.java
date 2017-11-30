5f9eafd680228da63a18748b16399f0a8f951a15
cice <cice@192.168.3.121>
2017-11-30 21:08:25 +0100

    Inicio proyecto

---
+++ src/com/cice/holamundo/Main.java
@@ -0,0 +1,14 @@
+
+package com.cice.holamundo;
+
+
+public class Main {
+    
+    public static void main (String [] args){
+        
+        // String... seria un array de string en java 8 y en Android
+        System.out.println("Hola mundo \u0007");
+        
+    }
+    
+}
